9f9b9d49022e5bfa2a2e92e33bbf9bd09642cb07
Ivan <ichromjak@gmail.com>
2014-11-30 16:55:40 +0100

    lang update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php if (!have_posts()) : ?>
 	<p class="no-results">
-		<?php _e('There were no results for your search! Try another search or use the links in header to locate what you’re looking for.', 'pressapps'); ?>
+		<?php _e('There were no results for your search! Try another search or use the links in header to locate what you are looking for.', 'pressapps'); ?>
 	</p>
 	<div class="not-found">
 		<?php get_template_part('templates/search', 'form'); ?>
